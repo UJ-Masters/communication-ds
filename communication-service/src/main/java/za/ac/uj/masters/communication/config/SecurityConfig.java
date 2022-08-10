684ea45f53de763618ab438dc95e755a95e00b7c
mkay47 <201402344@student.uj.ac.za>
2022-08-10 23:40:27 +0200

    added swagger and security

--- communication-service/src/main/java/za/ac/uj/masters/communication/config/SecurityConfig.java
+++ communication-service/src/main/java/za/ac/uj/masters/communication/config/SecurityConfig.java
@@ -5,28 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
-
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
-
-    /*@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .cors().and().authorizeRequests().antMatchers("/actuator/**", "/health/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                .and()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/sms/**", "/email/**")
-                .hasAuthority("SCOPE_write")
-                .anyRequest()
-                .authenticated()
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
-    }*/
+public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -42,7 +25,8 @@ public class SecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
                 .cors()
                 .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/sms/**", "/email/**")
+                .antMatchers(HttpMethod.POST,
+                        "/sms/**", "/email/**")
                 .hasAuthority("SCOPE_write")
                 .anyRequest()
                 .authenticated()
