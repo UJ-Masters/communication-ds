485bb6e0149c89214cbbd363bf6ea6ea7ebe94f2
mkay47 <201402344@student.uj.ac.za>
2022-11-17 13:31:47 +0200

    add concurrency

--- communication-service/src/main/java/za/ac/uj/masters/communication/rest/CommunicationController.java
+++ communication-service/src/main/java/za/ac/uj/masters/communication/rest/CommunicationController.java
@@ -1,5 +1,7 @@
 package za.ac.uj.masters.communication.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import za.ac.uj.masters.communication.model.EmailResponse;
@@ -8,10 +10,17 @@ import za.ac.uj.masters.communication.model.SmsResponse;
 import za.ac.uj.masters.communication.service.EmailService;
 import za.ac.uj.masters.communication.service.SmsService;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 @RestController
 @CrossOrigin("*")
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class CommunicationController {
+
+    private Logger logger = LoggerFactory.getLogger(CommunicationController.class);
 
     private final EmailService emailService;
     private final SmsService smsService;
@@ -24,13 +33,46 @@ public class CommunicationController {
 
     @PostMapping("/email")
     @ResponseBody
-    public EmailResponse sendEmail(@RequestBody SendRequest request) {
-        return emailService.sendEmail(request);
+    public EmailResponse sendEmail(@RequestBody SendRequest request) throws ExecutionException, InterruptedException {
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        try{
+            Future<EmailResponse> futureTask = threadPool.submit(() -> new EmailResponse(false));
+
+            while (!futureTask.isDone()) {
+                System.out.println("FutureTask is not finished yet...");
+            }
+            EmailResponse result = futureTask.get();
+
+            return emailService.sendEmail(request);
+
+        }catch (Exception exception){
+            logger.info(exception.getMessage());
+            throw exception;
+        }finally {
+            threadPool.shutdown();
+        }
+
     }
 
     @PostMapping("/sms")
     @ResponseBody
-    public SmsResponse sendSms(@RequestBody SendRequest request) {
-        return smsService.sendSms(request);
+    public SmsResponse sendSms(@RequestBody SendRequest request) throws ExecutionException, InterruptedException {
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        try{
+            Future<SmsResponse> futureTask = threadPool.submit(() -> new SmsResponse(false));
+
+            while (!futureTask.isDone()) {
+                System.out.println("FutureTask is not finished yet...");
+            }
+            SmsResponse result = futureTask.get();
+
+            return smsService.sendSms(request);
+
+        }catch (Exception exception){
+            logger.info(exception.getMessage());
+            throw exception;
+        }finally {
+            threadPool.shutdown();
+        }
     }
 }
--- communication-service/src/main/java/za/ac/uj/masters/communication/service/EmailService.java
+++ communication-service/src/main/java/za/ac/uj/masters/communication/service/EmailService.java
@@ -6,13 +6,19 @@ import org.springframework.stereotype.Component;
 import za.ac.uj.masters.communication.model.EmailResponse;
 import za.ac.uj.masters.communication.model.SendRequest;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 @Component
 public class EmailService {
     private final Logger log = LoggerFactory.getLogger(EmailService.class);
 
-    public EmailResponse sendEmail(SendRequest request) {
+    public EmailResponse sendEmail(SendRequest request)  {
         log.info("sending email");
         log.info(request.getMessage());
+
         return new EmailResponse(false);
     }
 }
